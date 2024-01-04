3411d29d2394d7f1f04dc4c489636ab55f1e0bb3
jjrpayne <jjrpayne@hotmail.com>
2024-01-03 18:50:22 -0800

    created index file and route/controller folders

---
+++ controllers/users.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const UsersController = require('../controllers/users');
+
+router.post('/auth/signup',
+);
+
+router.post('/auth/login',
+);
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 5001;
+
+// Allow Acces Control Allow Origin headers
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE"
+    );
+    next();
+});
+
+router = express.Router();
+app.use(router);
+
+app.use(bodyParsr.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use("/", require ("./routes/users"));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
